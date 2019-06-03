2af88605264eaad318eef2b6bce4a8f14c1dd495
Karim Slimani <karim@playmedia.fr>
2019-06-03 15:15:05 +0200

    chore: add Webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+// Webpack 4 configuration
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const NotifierPlugin = require('webpack-build-notifier')
+
+var name = 'clappr-ima-plugin'
+var outputFile, plugins = [], optimization = {}
+
+if (process.env.npm_lifecycle_event === 'dist') {
+  outputFile = name + '.min.js'
+  optimization.minimizer = [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+      }
+    }),
+  ]
+} else {
+  outputFile = name + '.js'
+  optimization.minimize = false
+}
+
+plugins.push(new NotifierPlugin({
+  title: optimization.minimizer ? 'minified ' + name : name,
+}))
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+    library: 'ClapprImaPlugin',
+    libraryTarget: 'umd',
+  },
+  optimization: optimization,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                path.resolve(__dirname, "node_modules/compass-mixins/lib"),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-inline-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: plugins,
+  externals: {
+   clappr: {
+    amd: 'clappr',
+    commonjs: 'clappr',
+    commonjs2: 'clappr',
+    root: 'Clappr'
+   }
+  },
+  devServer: {
+    contentBase: [
+      path.resolve(__dirname, "public"),
+    ],
+    // publicPath: '/js/',
+    disableHostCheck: true, // https://github.com/webpack/webpack-dev-server/issues/882
+    compress: true,
+    host: "0.0.0.0",
+    port: 8080
+  }
+}
